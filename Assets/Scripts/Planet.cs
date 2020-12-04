28efe1a890ee56f477e7d559e32d9dd7f9013313
Jordi <jroig24@gmail.com>
2020-12-04 23:28:38 +0100

    push test

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new plnet", menuName ="Jam/New planet")]
+public class Planet : ScriptableObject
+{
+    public Sprite planetSprite;
+
+
+
+    public float QuantitatPoblació = 0;
+    public regim Regim;
+    public int EdatEspecie;
+    public float EnergiaConsumida;
+    public tipus tipusPlaneta;
+    public float recursosConsumitsPerAny;
+    public float perillositat;
+    public float pastaGenerada;
+    public raca especie;
+
+
+    public enum raca { humans, reptilians, ewoks}
+    public enum tipus { primitiu, avancat, futurista }
+    public enum regim { democracia, dictadura, monarquia}
+}
