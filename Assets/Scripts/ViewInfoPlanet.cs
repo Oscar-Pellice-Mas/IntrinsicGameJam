59c822aa4209b82410dd130a412f03950bfe20de
saramj16 <sara.marti@students.salle.url.edu>
2020-12-05 00:24:51 +0100

    Info DIsplay

---
+++ Assets/Scripts/ViewInfoPlanet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewInfoPlanet : MonoBehaviour
+{
+    Planet planeta;
+
+    public GameObject imatge;
+    public GameObject nom;
+    public GameObject regim;
+    public GameObject edat;
+    public GameObject energia;
+    public GameObject tipus;
+    public GameObject recursos;
+    public GameObject perillositat;
+    public GameObject dineros;
+    public GameObject especie;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //imatge.GetComponents < Sprite > = planeta.planetSprite;
+       // nom.GetComponents<Text>().text = planeta.Nom;
+    }
+
+
+}
