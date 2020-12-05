5aef559407d60c2e274b2a30b95d7ca4a4ea8740
saramj16 <sara.marti@students.salle.url.edu>
2020-12-05 01:13:29 +0100

    Info Display Actualitzat

--- Assets/Scripts/ViewInfoPlanet.cs
+++ Assets/Scripts/ViewInfoPlanet.cs
@@ -1,29 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ViewInfoPlanet : MonoBehaviour
 {
-    Planet planeta;
+    public Planet planeta;
 
-    public GameObject imatge;
-    public GameObject nom;
-    public GameObject regim;
-    public GameObject edat;
-    public GameObject energia;
-    public GameObject tipus;
-    public GameObject recursos;
-    public GameObject perillositat;
-    public GameObject dineros;
-    public GameObject especie;
+    public Image imatge;
+    public TextMeshProUGUI nom;
+    public TextMeshProUGUI quantitatPoblacio;
+    public TextMeshProUGUI regim;
+    public TextMeshProUGUI edat;
+    public TextMeshProUGUI energia;
+    public TextMeshProUGUI tipus;
+    public TextMeshProUGUI recursos;
+    public TextMeshProUGUI perillositat;
+    public TextMeshProUGUI dineros;
+    public TextMeshProUGUI especie;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //imatge.GetComponents < Sprite > = planeta.planetSprite;
-       // nom.GetComponents<Text>().text = planeta.Nom;
+        imatge.sprite = planeta.planetSprite;
+        nom.text = planeta.Nom;    
+        quantitatPoblacio.text = planeta.QuantitatPoblació.ToString();
+        regim.text = planeta.Regim.ToString();
+        edat.text = planeta.EdatEspecie.ToString();
+        energia.text = planeta.EnergiaConsumida.ToString();
+        tipus.text = planeta.tipusPlaneta.ToString();
+        recursos.text = planeta.recursosConsumitsPerAny.ToString();
+        perillositat.text = planeta.perillositat.ToString();
+        dineros.text = planeta.pastaGenerada.ToString();
+        especie.text = planeta.especie.ToString();
+
     }
 
 
