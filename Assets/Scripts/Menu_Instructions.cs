753a58d4c4dd998c4a8ad196f5acaa9a611d34e0
saramj16 <sara.marti@students.salle.url.edu>
2020-12-08 11:54:03 +0100

    Canvis How To Play

--- Assets/Scripts/Menu_Instructions.cs
+++ Assets/Scripts/Menu_Instructions.cs
@@ -7,6 +7,9 @@ using UnityEngine.Audio;
 
 public class Menu_Instructions : MonoBehaviour
 {
+
+    public int current_page;
+
     public GameObject[] options;
 
     public Menu m;
@@ -17,6 +20,10 @@ public class Menu_Instructions : MonoBehaviour
 
     void Start()
     {
+        current_page = 0;
+        options[0].gameObject.SetActive(true);
+        options[1].gameObject.SetActive(false);
+        options[2].gameObject.SetActive(false);
         activaFons();
     }
 
@@ -36,9 +43,10 @@ public class Menu_Instructions : MonoBehaviour
 
     public void activaFons()
     {
-
-        options[0].transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().color = new Color(23f / 255f, 110f / 255f, 50f / 255f);
-        options[0].transform.GetChild(0).gameObject.SetActive(false);
+        options[3].transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().color = new Color(23f / 255f, 110f / 255f, 50f / 255f);
+        options[3].transform.GetChild(0).gameObject.SetActive(false);
+        options[4].gameObject.SetActive(false);
+        options[4].transform.GetChild(0).gameObject.SetActive(false);
 
     }
 
@@ -51,13 +59,43 @@ public class Menu_Instructions : MonoBehaviour
         else
         {
             isButtonPress = true;
-            killPlanetButton.SetBool("buttonDown", true);
-            yield return new WaitForSeconds(0.2f);
-            killPlanetButton.SetBool("buttonDown", false);
-            isButtonPress = false;
-            m.show_start();
+            switch (current_page)
+            {
+                case 0:
+                    killPlanetButton.SetBool("buttonDown", true);
+                    //Activar pantalla 2 i desctivar pantalla 1
+                    options[0].gameObject.SetActive(false);
+                    options[1].gameObject.SetActive(true);
+                    yield return new WaitForSeconds(0.2f);
+                    killPlanetButton.SetBool("buttonDown", false);
+                    isButtonPress = false;
+                    current_page++;
+                    break;
+                case 1:
+                    killPlanetButton.SetBool("buttonDown", true);
+                    options[1].gameObject.SetActive(false);
+                    options[2].gameObject.SetActive(true);
+                    yield return new WaitForSeconds(0.2f);
+                    killPlanetButton.SetBool("buttonDown", false);
+                    isButtonPress = false;
+                    options[3].gameObject.SetActive(false);
+                    options[4].gameObject.SetActive(true);
+                    options[4].transform.GetChild(1).gameObject.GetComponent<TMPro.TextMeshProUGUI>().color = new Color(23f / 255f, 110f / 255f, 50f / 255f);
+                    current_page++;
+                    break;
+                case 2:
+                    killPlanetButton.SetBool("buttonDown", true);
+                    yield return new WaitForSeconds(0.2f);
+                    killPlanetButton.SetBool("buttonDown", false);
+                    isButtonPress = false;
+                    m.show_start();
+                    current_page = 0;
+                    break;
+                default:
+                    break;
+            }
         }
-           
+
         yield return null;
     }
     
